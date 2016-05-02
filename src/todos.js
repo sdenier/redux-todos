f0b376069cf781f7c88ad8fa9fffbc396407cc84
Simon Denier <simon@sogilis.com>
2016-05-02 23:32:56 +0200

    Extract Todo/TodoList presentational components
    Lesson 20

--- src/todos.js
+++ src/todos.js
@@ -68,6 +68,28 @@ const FilterLink = ({ filter, currentFilter, children }) => {
   )
 }
 
+const Todo = ({onClick, completed, text}) => (
+  <li
+    onClick={onClick}
+    style={{
+      textDecoration: completed ? 'line-through' : 'none'
+    }}>
+    { text }
+  </li>
+)
+
+const TodoList = ({todos, onTodoClick}) => (
+  <ul>
+    {todos.map(todo =>
+      <Todo
+        key={todo.id}
+        {...todo}
+        onClick={() => onTodoClick(todo.id)}
+      />
+    )}
+  </ul>
+)
+
 const getVisibleTodos = (todos, filter) => {
   switch (filter) {
     case 'SHOW_ACTIVE':
@@ -103,22 +125,15 @@ class TodoApp extends React.Component {
         }}>
           Add Todo
         </button>
-        <ul>
-          {visibleTodos.map(todo =>
-            <li key={ todo.id }
-              onClick={() => {
-                store.dispatch({
-                  type: 'TOGGLE_TODO',
-                  id: todo.id
-                });
-              }}
-              style={{
-                textDecoration: todo.completed ? 'line-through' : 'none'
-              }}>
-              { todo.text }
-            </li>
-          )}
-        </ul>
+        <TodoList
+          todos={visibleTodos}
+          onTodoClick={id => {
+            store.dispatch({
+              type: 'TOGGLE_TODO',
+              id: id
+            });
+          }}
+        />
         <p>
           Show:&nbsp;
           <FilterLink filter='SHOW_ALL' currentFilter={visibilityFilter}>
