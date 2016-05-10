777145f83be6f291284a816591dead0624522043
Simon Denier <simon@sogilis.com>
2016-05-11 00:23:42 +0200

    Refactor with ReactRedux.Provider
    Lesson 26

--- src/todos.js
+++ src/todos.js
@@ -211,21 +211,7 @@ const TodoApp = () => (
   </div>
 )
 
-class Provider extends React.Component {
-  getChildContext() {
-    return {
-      store: this.props.store
-    }
-  }
-
-  render() {
-    return this.props.children
-  }
-}
-Provider.childContextTypes = {
-  store: React.PropTypes.object
-}
-
+const { Provider } = ReactRedux
 
 ReactDOM.render(
   <Provider store={ Redux.createStore(todoApp) }>
